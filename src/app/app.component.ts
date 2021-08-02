6ef648bd504216c1ff81231f7b68142c708d4e82
thomazAquino <thomazmanoel.aquino@gmail.com>
2021-08-02 12:18:41 -0300

    feat: Some header description 2 Some body 2
    Some footer 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'version-test 1....';
+  title = 'version-test 1.....';
 }
